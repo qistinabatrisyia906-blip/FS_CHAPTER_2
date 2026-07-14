0cf96bacd0d8159ce8d7fdb244deb3657051ec2b
Qistina <qistinabatrisyia906@gmail.com>
2026-07-14 16:27:58 +0800

    kelas week 3

---
+++ operators.php
@@ -0,0 +1,106 @@
+<?php
+
+//$a = 15;
+//$b = 4;
+
+//$x = $a + $b; //addition = 19
+//$x = $a - $b; //subtraction = 11
+//$x = $a / $b; //division = 3.75
+//$x = $a * $b; //multiplication = 60
+//$x = $a % $b; //modulus (reminder) =3
+//$x = $a ** $b; //exponentiation = 50,625
+
+//echo $x;
+
+/*$bottle = 5;
+$cost_per_bottle = 4;
+$discount = 2.00;
+$service_tax = 0.06;
+
+$total = $bottle * $cost_per_bottle;
+
+$total_after_discount = $total - $discount;
+
+$TotalSST = $total_after_discount * $service_tax;
+
+$finalTotal = $total_after_discount + $TotalSST;
+
+echo "Total before discount : RM " . $total . "<br>";
+echo "Total after discount : RM " . $total_after_discount . "<br>";
+echo "Total sst : RM" . $TotalSST . "<br>";
+echo "Final total : RM" . $finalTotal . "<br>";*/
+
+/*$a = 15; //assigns the value 15
+$a += 15; //addition assignment (x = x + 15)
+$a -= 15; //subtraction assignment (x = x - 15)
+$a *= 15; //multiplication assignment 
+$a /= 15; //division assignment
+$a %= 15; //modulus assignment
+
+echo $a; */
+
+/*echo "value x = 90 ; <br> value y = 90  <br><br>";
+$x = 90;
+$y = "90";
+
+
+
+if ($x != $y) {
+    echo "True";
+}
+else{
+    echo "False";
+}*/
+
+/*$age = 20;
+$hasLicense = false;
+
+//comparison : ($age >= 18)
+//logical: &&
+
+if ($age >= 18 || $hasLicense == true){
+    echo "you are allowed to drive.";
+}else {
+    echo "you are not allowed to drive";
+}*/
+
+/*$language = "id";
+$name = "qistina";
+
+if($language == 'en'){
+    echo "Hi, Good Morning "  . $name;
+} elseif ($language == 'my'){
+    echo "Hai, selamat pagi " . $name;
+
+} elseif ($language == 'ja'){
+    echo "Ohayou " . $name;
+} else {
+    echo "Hey " . $name;
+}*/
+
+
+/*$name = "Minah";
+$citizenship = "Malaysian";
+$age = 19;
+
+if ($citizenship == "Malaysian" && $age >=18){
+    echo "Eligible to vote";
+}else{
+    echo "Not eligible";
+}*/
+
+/*$age = 20;
+$status = ($age >= 18) ? "Eligible" : "Not Eligible";
+echo "Voting Status: " . $status; */
+
+$name1 = "Abu";
+$score1 = 75;
+
+$name2 = "Raj";
+$score2 = 85;
+
+
+
+
+
+?>
